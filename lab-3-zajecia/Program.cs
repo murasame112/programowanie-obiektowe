fe83dbd3853ae40a64a08675454be6d6c44558d6
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-03-23 13:06:31 +0100

    23.03 zajecia i zadania wykonane

---
+++ lab-3-zajecia/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace lab_3_zajecia
+{
+    class StackInt
+    {
+
+        private int[] _arr = new int[10];
+        private int _last = -1;
+        public void Push(int item)
+        {
+            _arr[++_last] = item;
+
+        }
+
+        public int Pop()
+        {
+            return _arr[_last--];
+        }
+    }
+    class Student
+    {
+        public int Egzam { get; set; }
+        public T GetReward<T>(T reward)
+        {
+            if(Egzam > 50)
+            {
+                return reward;
+            }
+            else
+            {
+                return default;
+            }
+        }
+    }
+
+
+    class Stack<T> /*where T: Student //wskazuje, ze typem moze byc tylko klasa student oraz klasy pochodne*/
+    {
+
+        private T[] _arr = new T[10];
+        private int _last = -1;
+        public void Push(T item)
+        {
+            _arr[++_last] = item;
+
+        }
+
+        public T Pop()
+        {
+            return _arr[_last--];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stackInt = new Stack<int>();
+            stackInt.Push(12);
+            stackInt.Push(13);
+            stackInt.Push(14);
+            stackInt.Push(15);
+            Console.WriteLine(stackInt.Pop());
+            Console.WriteLine(stackInt.Pop());
+            Console.WriteLine(stackInt.Pop());
+            Console.WriteLine(stackInt.Pop());
+
+            Stack<string> stackStr = new Stack<string>();
+
+            Student student = new Student() { Egzam = 45 };
+            Student student2 = new Student() { Egzam = 55 };
+            var stringReward = student.GetReward("Gratulacje");
+            var intReward = student2.GetReward<decimal>(100); //do GetReward nie musze podawac typu, ale moge to zrobic jesli potrzebuje
+            Console.WriteLine(intReward);
+            Console.WriteLine();
+
+            ValueTuple<string, decimal, int> product = ValueTuple.Create("laptop", 2000m, 3);
+
+            (string, decimal, int) laptop = ("laptop", 2000m, 3); //tak mozna zrobic bez wprowadzania klasy
+            Console.WriteLine(product == laptop);
+            Console.WriteLine();
+
+            product.Item1 = "cell phone";
+            Console.WriteLine(product == laptop);
+            Console.WriteLine();
+
+            (string name, decimal price, int quantity) anotherProduct = laptop;
+            Console.WriteLine($"name of product: {anotherProduct.name}");
+            Console.WriteLine();
+
+            anotherProduct = (name: "monitor", price: 1200m, quantity: 2); //mimo podania nazw to pola i tak musza byc w tej samej kolejnosci
+        }
+    }
+}
---
+++ lab-3/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace lab_3
+{
+    class App
+    {
+        public static void Main(string[] args)
+        {
+            //UWAGA!!! Nie usuwaj poniższego wiersza!!!
+            //Console.WriteLine("Otrzymałeś punktów: " + (Test.Exercises_1() + Test.Excersise_2() + Test.Excersise_3()));
+        }
+    }
+
+    //Ćwiczenie 1
+    //Zmodyfikuj klasę Musician, aby można było tworzyć muzyków dla T  pochodnych po Instrument.
+    //Zdefiniuj klasę  Guitar pochodną po Instrument, w metodzie Play zwróć łańcuch "GUITAR";
+    //Zdefiniuj klasę Drum pochodną po Instrument, w metodzie Play zwróć łańcuch "DRUM";
+    interface IPlay
+    {
+        string Play();
+    }
+
+    class Musician<T> : IPlay where T: Instrument
+    {
+        public T Instrument { get; init; }
+
+        public string Play()
+        {
+            return (Instrument as Instrument)?.Play();
+        }
+
+        public override string ToString()
+        {
+            return $"MUSICIAN with {(Instrument as Instrument)?.Play()}";
+        }
+    }
+
+    abstract class Instrument : IPlay
+    {
+        public abstract string Play();
+    }
+
+    class Keyboard : Instrument
+    {
+        public override string Play()
+        {
+            return "KEYBOARD";
+        }
+    }
+
+    class Guitar : Instrument
+    {
+        public override string Play()
+        {
+            return "GUITAR";
+        }
+
+    }
+
+    class Drum : Instrument
+    {
+        public override string Play()
+        {
+            return "DRUM";
+        }
+    }
+
+    //Cwiczenie 2
+    public class Exercise2<T>
+    {
+        //Zmień poniższą metodę, aby zwracała krotkę z polami typu string, int i bool oraz wartościami "Karol", 12 i true
+        public static object getTuple1()
+        {
+            (string, int, bool) obj = ("Karol", 12, true);
+
+            return obj;
+        }
+
+        //Zdefiniuj poniższą metodę, aby zwracała krotkę o dwóch polach
+        //firstAndLast: z tablicą dwuelementową z pierwszym i ostatnim elementem tablicy input
+        //isSame: z wartością logiczną określająca równość obu elementów
+        //dla pustej tablicy należy zwrócić krotkę z pustą tablica i wartościa false
+        //Przykład
+        //dla wejścia
+        //int[] arr = {2, 3, 4, 6}
+        //metoda powinna zwrócić krotkę
+        //var tuple = GetTuple2<int>(arr);
+        //tuple.firstAndLast    ==> {2, 6}
+        //tuple.isSame          ==> false
+
+        
+        public static ValueTuple<T[], bool> GetTuple2(T[] arr)
+        {
+             T[] firstAndLast = new T[2];
+            bool isSame;
+            firstAndLast[0] = arr[0];
+            firstAndLast[1] = arr[arr.Length - 1];
+            if (firstAndLast[0].Equals(firstAndLast[1]))
+            {
+                isSame = true;
+            }
+            else
+            {
+                isSame = false;
+            }
+            
+
+            ValueTuple<T[], bool> result = ValueTuple.Create(firstAndLast, isSame);
+
+            return result;
+        }
+        
+            
+
+    }
+       
+        
+            
+   
+    
+
+
+    //Cwiczenie 3
+    public class Exercise3
+    {
+        //Zdefiniuj poniższa metodę, która zlicza liczbę wystąpień elementów tablicy arr
+        //Przykład
+        //dla danej tablicy
+        //string[] arr = {"adam", "ola", "adam" ,"ewa" ,"karol", "ala" ,"adam", "ola"};
+        //wywołanie metody
+        //countElements(arr, "adam", "ewa", "ola");
+        //powinno zwrócić tablicę krotek
+        //{("adam", 3), ("ewa", 1), ("ola", 2)}
+        //co oznacza, że "adam" występuje 3 raz, "ewa" 1 raz a "ola" 2
+        //W obu tablicach moga pojawić się wartości null, które też muszą być zliczane
+        public static (T, int)[] countElements<T>(T?[] arr, params T?[] elements)
+        {
+
+            (T, int)[] result = new (T, int)[elements.Length];
+
+            int i = 0;
+            foreach (T element in elements)
+            {
+                int counter = 0;
+
+                foreach (T arrElement in arr)
+                {
+                    if (element.Equals(arrElement))
+                    {
+                        counter++;
+                    }
+                }
+                (T, int) resultTuple = (element, counter);
+                result[i] = resultTuple;
+                i++;
+            }
+
+            return result;
+            
+        }
+    }
+
+}
