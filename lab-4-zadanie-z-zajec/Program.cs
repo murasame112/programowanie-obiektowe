f9812d70474e5565fbd426382fc7917119c6785b
Tomasz Wiesek <tomasz.wiesek@microsoft.wsei.edu.pl>
2022-03-30 12:32:58 +0200

    30.03 ex1

---
+++ lab-4-zadanie-z-zajec/Program.cs
@@ -0,0 +1,149 @@
+﻿using System.Diagnostics.Tracing;
+using System.Drawing;
+using System;
+
+class App
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+enum Direction8
+{
+    UP = 1,
+    UP_RIGHT = 2,
+    RIGHT = 3,
+    DOWN_RIGHT = 4,
+    DOWN = 5,
+    DOWN_LEFT = 6,
+    LEFT = 7,
+    UP_LEFT = 8    
+}
+
+enum Direction4
+{
+    UP = 1,
+    RIGHT = 2,
+    DOWN = 3,
+    LEFT = 4,
+    
+}
+
+//Cwiczenie 1
+//Zdefiniuj metodę NextPoint, która powinna zwracać krotkę ze współrzędnymi piksela przesuniętego jednostkowo w danym kierunku względem piksela point.
+//Krotka screenSize zawiera rozmiary ekranu (width, height)
+//Przyjmij, że początek układu współrzednych (0,0) jest w lewym górnym rogu ekranu, a prawy dolny ma współrzęne (witdh, height) 
+//Pzzykład
+//dla danych wejściowych 
+//(int, int) point1 = (2, 4);
+//Direction4 dir = Direction4.UP;
+//var point2 = NextPoint(dir, point1);
+//w point2 powinny być wartości (2, 3);
+//Jeśli nowe położenie jest poza ekranem to metoda powinna zwrócić krotkę z point
+class Exercise1
+{
+    public static (int, int) NextPoint(Direction4 direction, (int, int) point, (int, int) screenSize)
+    {
+        (int, int) resultPoint = point;
+        resultPoint = direction switch
+        {
+            Direction4.UP => (resultPoint.Item1, resultPoint.Item2 - 1),
+            Direction4.RIGHT => (resultPoint.Item1 + 1, resultPoint.Item2),
+            Direction4.DOWN => (resultPoint.Item1, resultPoint.Item2 + 1),
+            Direction4.LEFT => (resultPoint.Item1 - 1, resultPoint.Item2)
+        };
+
+        if((resultPoint.Item1 > screenSize.Item1) || (resultPoint.Item2 > screenSize.Item2))
+        {
+            return point;
+        }
+        else 
+        {
+            return resultPoint;
+        }
+
+        
+    }
+
+}
+//Cwiczenie 2
+//Zdefiniuj metodę DirectionTo, która zwraca kierunek do piksela o wartości value z punktu point. W tablicy screen są wartości
+//pikseli. Początek układu współrzędnych (0,0) to lewy górny róg, więc punkt o współrzęnych (1,1) jest powyżej punktu (1,2) 
+//Przykład
+// Dla danych weejsciowych
+// static int[,] screen =
+// {
+//    {1, 0, 0},
+//    {0, 0, 0},
+//    {0, 0, 0}
+// };
+// (int, int) point = (1, 1);
+// po wywołaniu - Direction8 direction = DirectionTo(screen, point, 1);
+// w direction powinna znaleźć się stała UP_LEFT
+class Exercise2
+{
+    static int[,] screen =
+    {
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    private static (int, int) point = (1, 1);
+
+    private Direction8 direction = DirectionTo(screen, point, 1);
+
+    public static Direction8 DirectionTo(int[,] screen, (int, int) point, int value)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+//Cwiczenie 3
+//Zdefiniuj metodę obliczającą liczbę najczęściej występujących aut w tablicy cars
+//Przykład
+//dla danych wejściowych
+// Car[] _cars = new Car[]
+// {
+//     new Car(),
+//     new Car(Model: "Fiat", true),
+//     new Car(),
+//     new Car(Power: 100),
+//     new Car(Model: "Fiat", true),
+//     new Car(Power: 125),
+//     new Car()
+// };
+//wywołanie CarCounter(Car[] cars) powinno zwrócić 3
+record Car(string Model = "Audi", bool HasPlateNumber = false, int Power = 100);
+
+class Exercise3
+{
+    public static int CarCounter(Car[] cars)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+record Student(string LastName, string FirstName, char Group, string StudentId = "");
+//Cwiczenie 4
+//Zdefiniuj metodę AssignStudentId, która każdemu studentowi nadaje pole StudentId wg wzoru znak_grupy-trzycyfrowy-numer.
+//Przykład
+//dla danych wejściowych
+//Student[] students = {
+//  new Student("Kowal","Adam", 'A'),
+//  new Student("Nowak","Ewa", 'A')
+//};
+//po wywołaniu metody AssignStudentId(students);
+//w tablicy students otrzymamy:
+// Kowal Adam 'A' - 'A001'
+// Nowal Ewa 'A'  - 'A002'
+//Należy przyjąc, że są tylko trzy możliwe grupy: A, B i C
+class Exercise4
+{
+    public static void AssignStudentId(Student[] students)
+    {
+        
+    }
+}
---
+++ lab-4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab_4
+{
+
+    enum Degree
+    {
+        A = 50,
+        B = 45,
+        C = 40,
+        D = 35,
+        E = 30,
+        F = 20
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Degree degree = Degree.F;
+            Console.WriteLine((int)degree);
+            string[] vs = Enum.GetNames<Degree>();
+            Degree[] degrees = Enum.GetValues<Degree>();
+            Console.Write("ocena: ");
+            string str = Console.ReadLine();
+            try
+            {
+                Degree studentDegree = Enum.Parse<Degree>(str);
+                Console.WriteLine("Ocena: " + studentDegree);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Zla ocena");
+            }
+            double ocena = degree switch
+            {
+                Degree.A => 5.0,
+                Degree.B => 4.5,
+                Degree.C or Degree.D => 4.0,
+                _ => 3.0
+            };
+        }
+    }
+}
